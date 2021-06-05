c435a51bde641a9718e1485d715a09dc6ce4e85d
smlethil <40178928+Suanlianmang@users.noreply.github.com>
2021-06-06 01:29:37 +0530

    Update forms.py

--- User/forms.py
+++ User/forms.py
@@ -1,16 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
-from phonenumber_field.formfields import PhoneNumberField
 from .models import CHOICE, Profile
 
 class UserLoginForm(forms.ModelForm):
-	phone_number = PhoneNumberField(widget=forms.TextInput(attrs={}), label='Phone number')
 	class Meta:
 		model = User
 		fields = ['phone_number', 'password']
 
 class UserRegisterForm(forms.ModelForm):
-	phone_number = PhoneNumberField(widget=forms.TextInput(attrs={}), label='Phone number')
 	user_type = forms.ChoiceField(choices=CHOICE)
 	class Meta:
 		model = Profile
